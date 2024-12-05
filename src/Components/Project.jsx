46d85011f605f7ffeaf8eccdd5ffc10edfa1eefe
Rohit Patil <rohitpatil652001@gmail.com>
2024-12-05 11:47:02 +0530

    add New Porject in project section

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import nike from "../assets/Projects/Nike.png";
 import ID from "../assets/Projects/interior.png";
+import foodDevlivery from "../assets/Projects/fooddefinal.png";
 
 export default function Project() {
   let [data, setData] = useState([
@@ -16,6 +17,12 @@ export default function Project() {
       heading: "Interior Design",
       info: "It is a website that showcases interior designs created by designers. It also includes offers and promotions for clients to explore various design options.",
     },
+    {
+      id: "3",
+      image: foodDevlivery,
+      heading: "Burger Delight E-Commerce",
+      info: "Created a fully responsive e-commerce website featuring a dynamic burger cart. Used Redux Toolkit for real-time cart updates and Tailwind CSS for sleek styling, ensuring a seamless user experience.",
+    },
   ]);
 
   return (
@@ -26,12 +33,12 @@ export default function Project() {
           return (
             <div
               key={e.id}
-              className="w-full md:w-1/2 border border-black p-5 rounded-lg shadow-lg"
+              className="w-full  md:w-1/2 border border-black p-5 rounded-lg shadow-lg"
             >
               <img
                 src={e.image}
                 alt={e.heading}
-                className="w-full  object-cover rounded-lg mb-5"
+                className="w-full  object-cover rounded-lg mb-5 "
               />
               <h5 className="text-2xl md:text-3xl lg:text-2xl  font-semibold mb-3">
                 {e.heading}
